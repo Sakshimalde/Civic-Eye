5419d29f3ab8277875729141722c7c2206ee1138
Arnav77777 <arnavdeshpande70@gmail.com>
2025-11-18 17:13:27 +0530

    vite removed and prep for deployment

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,   // ✅ Force Vite to use port 3000
-  },
-})
